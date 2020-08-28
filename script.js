4c83a2c3fd287a74fa73f68c393dd9ae0af342bd
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2020-08-27 23:47:56 -0400

    Add variables to target several DOM elements

--- script.js
+++ script.js
@@ -2,9 +2,24 @@
 
 const balanceElement = document.getElementById("balance");
 
-console.log(balanceElement);
-
 // Value Input
+
+const valueInputElement = document.querySelector("input");
+
 // Income/Expense
+
+const selectElement = document.querySelector("select");
+
 // Addition Button
+
+const additionButtonElement = document.querySelector("footer button");
+
 // Entry
+
+const entryListElement = document.querySelector("section ul");
+
+console.log(balanceElement);
+console.log(valueInputElement);
+console.log(selectElement);
+console.log(additionButtonElement);
+console.log(entryListElement);
